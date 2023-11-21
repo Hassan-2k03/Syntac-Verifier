61c8d9bd6118cd077f2e2c0e9e8f183127811177
Mohammed Hassan <mh1921544@gmail.com>
2023-11-21 12:24:53 +0530

    Add lexer and parser for isalpha function

---
+++ isalpha/lex.py
@@ -0,0 +1,28 @@
+# lex.py
+from ply import lex
+
+tokens = (
+    'CHARACTER',
+    'LPAREN',
+    'RPAREN',
+    'SEMICOLON',
+    'ISALPHA',
+    'QUOTE',
+)
+
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_SEMICOLON = r';'
+t_ISALPHA = r'isalpha'
+t_CHARACTER = r'\'[a-zA-Z]\''
+
+
+
+# Error handling rule
+def t_error(t):
+    print(f"Error: Invalid character '{t.value[0]}'")
+    t.lexer.skip(1)
+
+
+
+lexer = lex.lex()
